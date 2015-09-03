90781842f3cd19e54d2455281b8e1b1ddceb0db6
Micah Warren <mwarren@basho.com>
2015-09-03 17:11:00 -0600

    Added support for node module-ness.

--- src/dice.js
+++ src/dice.js
@@ -1,71 +1,84 @@
-dice = {
-	version: "0.7.0",
+var dice = {};
+var parse = require('./parser.js');
+var eval = require('./evaluate.js');
+dice.parse = parse.parse;
+dice.eval = eval.eval;
+dice.version = "0.7.0";
 
-	roll: function(str, scope){
-		var parsed = dice.parse.parse(str);
-		var evaled = dice.eval.eval(parsed, scope);
-		return evaled;
-	},
+function roll(str, scope){
+	var parsed = parse.parse(str);
+	var evaled = eval.eval(parsed, scope);
+	return evaled;
+};
 
-	statistics: function(str, scope, samples){
-		if(typeof(scope) == "number"){
-			samples = scope;
-			scope = {};
-		}
-		scope = scope || {};
-		samples = samples || 1000;
-		var resultSet = [];
-		var i;
-		for(i = 0; i < samples; i++){
-			resultSet.push(dice.roll(str, scope));
-		}
-		var mean = resultSet.reduce(function(n, acc){ return n + acc; }, 0) / samples;
-		var min = resultSet.reduce(function(n, acc){ return n < acc ? n : acc; }, resultSet[0]);
-		var max = resultSet.reduce(function(n, acc){ return n > acc ? n : acc; }, resultSet[0]);
-		return {
-			'results': resultSet,
-			'mean': mean,
-			'min': parseInt(min.toFixed()),
-			'max': parseInt(max.toFixed())
-		};
-	},
+dice.roll = roll;
 
-	stringify_expression: function(evaled_op){
-		var sub = dice.stringify(evaled_op.expression);
-		var prefix = evaled_op.op[0];
-		if(prefix === 'p'){
-			prefix = '';
-		}
-		
-		return prefix + "( " + sub + " )";
-	},
+dice.statistics = function(str, scope, samples){
+	if(typeof(scope) == "number"){
+		samples = scope;
+		scope = {};
+	}
+	scope = scope || {};
+	samples = samples || 1000;
+	var resultSet = [];
+	var i;
+	for(i = 0; i < samples; i++){
+		resultSet.push(roll(str, scope));
+	}
+	var mean = resultSet.reduce(function(n, acc){ return n + acc; }, 0) / samples;
+	var min = resultSet.reduce(function(n, acc){ return n < acc ? n : acc; }, resultSet[0]);
+	var max = resultSet.reduce(function(n, acc){ return n > acc ? n : acc; }, resultSet[0]);
+	return {
+		'results': resultSet,
+		'mean': mean,
+		'min': parseInt(min.toFixed()),
+		'max': parseInt(max.toFixed())
+	};
+};
 
-	stringify_op: function(evaled_op){
-		var rs = dice.stringify(evaled_op.rightSide);
-		var ls = dice.stringify(evaled_op.leftSide);
-		return rs + ' ' + evaled_op.op + ' ' + ls;
-	},
+function stringify_expression(evaled_op){
+	var sub = stringify(evaled_op.expression);
+	var prefix = evaled_op.op[0];
+	if(prefix === 'p'){
+		prefix = '';
+	}
+	
+	return prefix + "( " + sub + " )";
+};
 
-	stringify_rolls: function(evaled_roll){
-		var minStr = evaled_roll.min > 1 ? evaled_roll.min + '..' : '';
-		var preamble = evaled_roll.x + evaled_roll.mode + minStr + evaled_roll.max + ':[';
-		return preamble + evaled_roll.rolls.join(', ') + ']';
-	},
+function stringify_op(evaled_op){
+	var rs = stringify(evaled_op.rightSide);
+	var ls = stringify(evaled_op.leftSide);
+	return rs + ' ' + evaled_op.op + ' ' + ls;
+};
 
-	stringify: function(evaled){
-		if(evaled.expression){
-			return dice.stringify_expression(evaled);
-		}
+function stringify_rolls(evaled_roll){
+	var minStr = evaled_roll.min > 1 ? evaled_roll.min + '..' : '';
+	var preamble = evaled_roll.x + evaled_roll.mode + minStr + evaled_roll.max + ':[';
+	return preamble + evaled_roll.rolls.join(', ') + ']';
+};
 
-		if(evaled.op){
-			return dice.stringify_op(evaled);
-		}
-
-		if(evaled.rolls){
-			return dice.stringify_rolls(evaled);
-		}
-
-		return evaled.toString();
+function stringify(evaled){
+	if(evaled.expression){
+		return stringify_expression(evaled);
 	}
 
+	if(evaled.op){
+		return stringify_op(evaled);
+	}
+
+	if(evaled.rolls){
+		return stringify_rolls(evaled);
+	}
+
+	return evaled.toString();
 };
+
+dice.stringify = stringify;
+
+var k;
+for(k in dice){
+    exports[k] = dice[k];
+}
+
+return dice;
